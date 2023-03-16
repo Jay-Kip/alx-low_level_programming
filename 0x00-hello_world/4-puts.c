c6a785d1acad73c6c4325076e8edf1b5f78ae36b
Jay <hanningtonkiptoo14@gmail.com>
2023-03-16 18:54:55 +0000

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-put("Programming is like building a multilingual puzzle\n"); return 0;
+#include <stdio.h>
+/**
+  * main - A program that prints a line using the printf function
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
