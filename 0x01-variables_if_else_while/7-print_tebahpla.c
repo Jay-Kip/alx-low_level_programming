a443c7843b75d9e0527a41c40cbe527c7b453306
Jay <hanningtonkiptoo14@gmail.com>
2023-03-17 20:35:57 +0000

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Return: zero
+*/
+int main(void)
+{
+	char j;
+
+	for (j = 'z'; j >= 'a'; j--)
+	{
+	putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
