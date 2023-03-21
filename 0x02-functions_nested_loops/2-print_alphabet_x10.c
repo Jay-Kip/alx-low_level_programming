948e3b91725399e2a4164e53faac14a10f21416b
Jay <hanningtonkiptoo14@gmail.com>
2023-03-21 16:36:47 +0000

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print 'a' to 'z' 10 times
+*
+* Return: zero
+*/
+void print_alphabet_x10(void)
+{
+	char k;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		k = 'a';
+		while (k <= 'z')
+		{
+		_putchar(k);
+			k++;
+		}
+		i++;
+	}
+}
