4fcdb2521b0d41b0061172e5ee0a828278e176b0
Jay <hanningtonkiptoo14@gmail.com>
2023-03-31 12:33:06 +0000

    9. Numbers have life; they're not just symbols on paper

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_number - entry
+* @n: int
+* Return: zero
+*/
+int print_number(int n)
+{
+	int i = n;
+
+	while (i != '\0')
+	{
+		if (i >= 0 && i <= 1000)
+		{
+			i = i;
+		}
+		i++;
+	}
+	return (i);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -10,7 +10,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
+int print_number(int n);
 
 
 #endif
