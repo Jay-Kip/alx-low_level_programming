42711cd684efdd97332dabb8dd4fe01602a98748
Jay <hanningtonkiptoo14@gmail.com>
2023-04-05 17:10:24 +0000

    3. You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* factorial - entry point of factorial
+* @n: incomirg integer
+* Return: factorized int
+*/
+int factorial(int n)
+{
+	if (n == 1)
+	{
+		return (1);
+	}
+	else if (n <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return n * factorial(n - 1);
+	}
+}
+
