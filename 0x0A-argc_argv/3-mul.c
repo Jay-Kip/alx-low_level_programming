25630bc8e5b3778adc46a6c42d477c926cc2dcba
Jay <hanningtonkiptoo14@gmail.com>
2023-04-09 05:25:50 +0000

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
-/*
+#include <math.h>
+#include <stdlib.h>
+/**
+* main - main function
+* @argc: arguement count
+* @argv: arguement vector
+* Return: zero
+*/
+int main(int argc, char *argv[])
+{
+
+	int k = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n",k);
+	return (0);
+}
