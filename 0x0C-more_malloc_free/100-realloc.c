ea2c6203eb115c458b55d37dcf7b2846f6ca630d
Jay <hanningtonkiptoo14@gmail.com>
2023-04-13 21:15:11 +0000

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (new_size == 0 && new_size == 0)
+	if (new_size == 0 && 0)
 	{
 		free(ptr);
 		return (NULL);
