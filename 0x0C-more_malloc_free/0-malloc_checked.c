b379de592acd2a067bed02f636dd6795da7a8933
Jay <hanningtonkiptoo14@gmail.com>
2023-04-13 11:57:07 +0000

    Trust no one:)

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,13 +7,14 @@
 */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *p;
 
-	ptr = malloc(b);
+	p = malloc(b);
 
-	if (ptr == NULL)
+	if (p == NULL)
 	{
 		exit(98);
 	}
-	return (ptr);
+
+	return (p);
 }
