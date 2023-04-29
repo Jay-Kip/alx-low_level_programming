6c7660f722a2742213480385f05d821384c8d34e
Jay <hanningtonkiptoo14@gmail.com>
2023-04-29 19:07:19 +0000

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+* free_listint - function to free a linked list
+* @head: pointer to the linked list
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while (head)
+	{
+		node = head->next;
+		free(node);
+		head = node;
+	}
+}
