1c64e77c0c04e02f9ca2c719b2338ed8e116b6c2
Jay <hanningtonkiptoo14@gmail.com>
2023-05-02 21:59:05 +0000

    12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -58,7 +58,7 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		for (; head != NULL; x++)
 		{
-			printf("[%p]%d\n", (void *)head, head->n);
+			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
 		}
 	}
