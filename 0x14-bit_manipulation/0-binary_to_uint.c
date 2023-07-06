81a6df10dc18fa483477e00d4a24c4064091bd3e
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-07-06 13:53:11 +0000

    a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,15 @@
  **/
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
+	unsigned int z = 0;
 	int i;
 
 	if (!b)
-	{
 		return (0);
-	}
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
-	
-			return (0);
-		n = 2 * n + (b[i] - '0');
+		z = 2 * z + (b[i] - '0');
 	}
-	return (n);
+	return (z);
 }
