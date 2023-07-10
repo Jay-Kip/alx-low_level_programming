7ccf4a2aeee3771fb542dc3b1a8944ba0139ab9c
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-07-10 20:26:06 +0000

    a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,8 +12,8 @@ char *create_b(char *file);
 
 int main(int argc, char *argv[])
 {
-	int from, to, r, w;
-	char *buffer;
+	int f, t, b, m;
+	char *a;
 
 	if (argc != 3)
 	{
@@ -21,41 +21,41 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buffer = create_b(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	a  = create_b(argv[2]);
+	f = open(argv[1], O_RDONLY);
+	b = read(f, a, 1024);
+	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from == -1 || r == -1)
+		if (f == -1 || b == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
+			free(a);
 			exit(98);
 		}
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		m = write(t, a, b);
+		if (t == -1 || m == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			free(buffer);
+			free(a);
 			exit(99);
 		}
 
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-	} while (r > 0);
+		b = read(f, a, 1024);
+		t = open(argv[2], O_WRONLY | O_APPEND);
+	} while (b > 0);
 
-	free(buffer);
-	close_f(from);
-	close_f(to);
+	free(a);
+	close_f(f);
+	close_f(t);
 
 	return (0);
 }
 
 /**
-* create_b - funtion to create buffer
-* @file: file
-* Return: copy
+* create_b - funtion to create buffer of 1024 bytes
+* @file: file name
+* Return: pointer to the new buffer
 */
 
 char *create_b(char *file)
@@ -77,15 +77,15 @@ char *create_b(char *file)
  */
 
 
-void close_f(int fd)
+void close_f(int dk)
 {
 	int t;
 
-	t = close(fd);
+	t = close(dk);
 
 	if (t == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dk);
 		exit(100);
 	}
 }
