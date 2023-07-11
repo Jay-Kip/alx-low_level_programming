a0ef269a3b4525bb54716eff9434a72344b7c642
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-07-11 00:06:56 +0000

    ubuntu

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -23,11 +23,9 @@ void close_elf(int elf);
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
-
 	int o, r;
-
+	
 	o = open(argv[1], O_RDONLY);
-
 	if (o == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
