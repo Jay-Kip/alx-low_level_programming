0027bd0bd07f4e8af1ff5845ce5bc743b61430cc
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-17 11:58:48 +0000

    Function that adds a node at a given index in a linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,39 +10,48 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current_node = *h;
-	unsigned int i = 0;
 	dlistint_t *new_node;
+	dlistint_t *current_node;
+	unsigned int i;
 
-	new_node = malloc(sizeof(dlistint_t));
-
+	new_node = NULL;
 	if (idx == 0)
 		new_node = add_dnodeint(h, n);
 
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-
-	while (current_node != NULL)
+	else
 	{
-		if (i == idx)
+
+		current_node = *h;
+		i = 1;
+
+		if (current_node != NULL)
+			while (current_node->prev != NULL)
+				current_node = current_node->prev;
+
+		while (current_node != NULL)
 		{
-			new_node->prev = current_node;
-			new_node->next = current_node->next;
+			if (i == idx)
+			{
+				if (current_node->next == NULL)
+					new_node = add_dnodeint_end(h, n);
+				else
+				{
+					new_node = malloc(sizeof(dlistint_t));
 
-			if (new_node == NULL)
-				return (NULL);
-
-			if (current_node->next != NULL)
-				current_node->next->prev = new_node;
-
-		current_node->next = new_node;
-		return (new_node);
+					if (new_node != NULL)
+					{
+						new_node->n = n;
+						new_node->next = current_node->next;
+						new_node->prev = current_node;
+						current_node->next->prev = new_node;
+						current_node->next = new_node;
+					}
+				}
+				break;
+			}
+			current_node = current_node->next;
+			i++;
 		}
-		current_node = current_node->next;
-		i++;
 	}
-	free(new_node);
-	return (NULL);
+	return (new_node);
 }
