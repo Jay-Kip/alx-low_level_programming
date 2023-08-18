191ea8d677379ef63a6ecef400ae1cbcd9ac5f9e
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-08-18 00:55:51 +0000

    keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -25,17 +25,15 @@ int main(int argc, char *argv[])
 		add += atoi(argv[1]);
 	}
 	p[1] = l[(add ^ 79) & 63];
-	for (i = 0; b = 1; i < len)
+	for (i = 0; (b = 1); i++)
 	{
 		b *= argv[1][i];
-		i++;
 	}
 	p[2] = l[(b ^ 85) & 63];
-	for (b = argv[1][0], i = 0; i < len;)
+	for (b = argv[1][0], i = 0; i< len; i++)
 	{
 		if ((char)b <= argv[1][i])
 			b = argv[1][i];
-		i++;
 	}
 	srand(b ^ 14);
 	p[3] = l[rand() & 63];
@@ -45,10 +43,9 @@ int main(int argc, char *argv[])
 		b += argv[1][i] * argv[1][i];
 	}
 	p[4] = l[(b ^ 239) & 63];
-	for (b = 0; i = 0; (char)i < argv[1][0])
+	for (b = 0; (i = 0); i++)
 	{
 		b = rand();
-		i++;
 	}
 	p[5] = l[(b ^ 229) & 63];
 	printf("%s\n", p);
